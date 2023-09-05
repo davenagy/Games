ded1e8cfdb69ea8b858410426a96f9b1207d0fef
David Nagy <144080262+davenagy@users.noreply.github.com>
2023-09-04 20:07:34 -0700

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -3,6 +3,10 @@ import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
+test_surface = pygame.Surface((100,200))
+test_surface.fill((0,0,255))
+test_rect = test_surface.get_rect(center = (200,250))
+
 
 while True:
     for event in pygame.event.get():
@@ -10,6 +14,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # draw all out elements
+    screen.fill((175,215,70))
+    pygame.draw.ellipse(screen,pygame.Color('red'), test_rect)
+    screen.blit(test_surface,test_rect)
     pygame.display.update()
     clock.tick(60)
